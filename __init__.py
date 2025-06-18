74a187fdf82e1668f0b47fe4287953b8377cb49a
guilherme <guilhermepfm@gmail.com>
2025-06-18 10:24:36 -0300

    [fix] error in importing

--- __init__.py
+++ __init__.py
@@ -1,6 +1,7 @@
-from .config import app
-from .config import user_tag
-from .config import home_tag
-from .config import transaction_tag
-from .config import transaction_category_tag
-from .config import transaction_type_tag
+# from .config import app
+# from .config import user_tag
+# from .config import home_tag
+# from .config import transaction_tag
+# from .config import transaction_category_tag
+# from .config import transaction_type_tag
+# # from .machine_learning_models.transactions_classifier import TransactionsClassifier
