023b8d5ac6bc916e13140c5af51b7a419018e002
guilherme <guilhermepfm@gmail.com>
2025-12-15 19:30:53 -0300

    fix: update embedding API request format in external_embedding_api.py
    - Modified the structure of the JSON payload sent to the embedding API to include a top-level 'descriptions' key, ensuring compatibility with the API's expected input format.
    - Changed the request to use `json.dumps` for proper serialization of the payload.

--- machine_learning/transactions_classification/lib/external_embedding_api.py
+++ machine_learning/transactions_classification/lib/external_embedding_api.py
@@ -1,11 +1,12 @@
 import requests
 import os
+import json
 
 EMBEDDING_API_URL = os.getenv('EMBEDDING_API_URL')
 
 def create_embeddings_api(array_of_texts):
-    descriptions = [{'description': text} for text in array_of_texts]
-    response = requests.post(f"{EMBEDDING_API_URL}/embeddings", json=descriptions)
+    descriptions = {'descriptions':[{'description': text} for text in array_of_texts]}
+    response = requests.post(f"{EMBEDDING_API_URL}/embeddings", json=json.dumps(descriptions))
     if response.status_code != 200:
         raise Exception(f"Failed to create embeddings: {response.text}")
     return response.json()['embeddings']
